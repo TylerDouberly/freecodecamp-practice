5e77dbd19711ec2541f276fc50a7d7d4de941235
TylerDouberly <62620034+TylerDouberly@users.noreply.github.com>
2022-06-10 18:08:40 +0000

    added Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -19,10 +19,24 @@ app.get('/now', function(req,res, next){
 app.get("/",(req, res)=>{
   res.sendFile(__dirname + "/views/index.html");
 });
-
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
 app.use("/public",express.static(__dirname +"/public"));
 
-
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 app.get("/json", (req, res)=>{
   const mySecret = process.env['MESSAGE_STYLE']
   if(mySecret == "uppercase") {
